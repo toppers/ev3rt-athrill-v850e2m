5147e00928ff6cebea070504e484866b66448acb
tmori@esm.co.jp <tmori@esm.co.jp>
2019-10-14 15:30:41 +0900

    add ev3 sources for asp3

---
+++ asp3/target/v850_gcc/pil/include/driver_svc.h
@@ -0,0 +1,16 @@
+#ifndef _DRIVER_SVC_H_
+#define _DRIVER_SVC_H_
+
+/*
+ *  cal_svcサービスコールのインタフェース
+ */
+Inline ER_UINT
+cal_svc(FN fncd, intptr_t par1, intptr_t par2,
+							intptr_t par3, intptr_t par4, intptr_t par5)
+{
+	//TODO
+	return(E_RSFN);
+}
+
+
+#endif
