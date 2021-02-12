ad1fef4c29b06cea3bfcf0088071b00271bef1fa
Takashi Mori <tmori@esm.co.jp>
2021-02-12 14:33:41 +0900

    add bt sample with linetrace

--- sdk/workspace/bt_sample/app.h
+++ sdk/workspace/bt_sample/app.h
@@ -14,6 +14,7 @@ extern "C" {
 #ifndef TOPPERS_MACRO_ONLY
 
 extern void main_task(intptr_t exinf);
+extern void bt_task(intptr_t exinf);
 extern void tracer_task(intptr_t exinf);
 extern void tracer_cyc(intptr_t exinf);
 
