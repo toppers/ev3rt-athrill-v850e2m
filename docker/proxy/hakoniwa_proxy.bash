63b69347123b0edf75a033f78bb81bd4dafaef8b
Takashi Mori <tmori@esm.co.jp>
2021-03-08 15:13:52 +0900

    hakoniwa proxy ipaddr auto

--- docker/proxy/hakoniwa_proxy.bash
+++ docker/proxy/hakoniwa_proxy.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-COUNT=0
+RESOLVE_IPADDR=`cat /etc/resolv.conf | grep nameserver | awk '{print $2}'`
 while [ 1 ]
 do
-        echo "############ START CONNECT:${CONNECT} #################"
-        ./bin/hakoniwa_proxy ./proxy_param.json 172.27.160.1 50051
-        echo "############ SERVER DOWN :${CONNECT} #################"
+        echo "############ START CONNECT #################"
+        ./bin/hakoniwa_proxy ./proxy_param.json ${RESOLVE_IPADDR}  50051
+        echo "############ SERVER DOWN #################"
         sleep 5
 done
