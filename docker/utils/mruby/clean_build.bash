7100da870ab73dbd37f0a91c165fb0ee9e4ebd0f
Takashi Mori <tmori@esm.co.jp>
2021-03-13 12:48:18 +0900

    add mruby build

--- docker/utils/mruby/clean_build.bash
+++ docker/utils/mruby/clean_build.bash
@@ -5,10 +5,16 @@ then
 	echo "Usage: $0 <apl-name>"
 	exit 1
 fi
-EV3RT_DIR=ev3rt-athrill-v850e2m/sdk/workspace
+MRUBY_BIN=/root/workspace/mruby/bin
+EV3RT_DIR=/root/workspace/ev3rt-athrill-v850e2m/sdk/workspace
 APL_NAME=${1}
 
 cd ${EV3RT_DIR}
+cd ${APL_NAME}
+rm -f main_task.h
+${MRUBY_BIN}/mrbc -g -v -Bbcode -omain_task.h main_task.rb > /dev/null
+ruby /root/workspace/mruby/delete_extern.rb
+cd ..
 
 make img=${APL_NAME}
 
