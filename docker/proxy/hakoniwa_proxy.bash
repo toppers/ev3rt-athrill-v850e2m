83dd40846780efcd45c28bc0b652b9e6cc5257ab
Takashi Mori <tmori@esm.co.jp>
2021-04-04 12:35:46 +0900

    add hakoniwa command

--- docker/proxy/hakoniwa_proxy.bash
+++ docker/proxy/hakoniwa_proxy.bash
@@ -4,7 +4,7 @@ RESOLVE_IPADDR=`cat /etc/resolv.conf | grep nameserver | awk '{print $2}'`
 while [ 1 ]
 do
         echo "############ START CONNECT #################"
-        ./bin/hakoniwa_proxy ./proxy_param.json ${RESOLVE_IPADDR}  50051
+        hakoniwa_proxy ./proxy_param.json ${RESOLVE_IPADDR}  50051
         echo "############ SERVER DOWN #################"
         sleep 1
 done
--- docker/proxy/start_proxy.bash
+++ docker/proxy/start_proxy.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export PATH=/root/hakoniwa-core/impl/asset/client/build/cpp:${PATH}
+cd /root/workspace 
+
 bash config/create_config.bash base_practice_1 eth0
 
 cd proxy
