7848042c71d6b2d4e9de255e31f0c9bde194053d
森崇 <tmori@moritakashinoMacBook-Air.local>
2021-05-05 11:20:06 +0900

    add native mac proxy

--- docker/proxy/hakoniwa_proxy-mac.bash
+++ docker/proxy/hakoniwa_proxy-mac.bash
@@ -4,7 +4,7 @@ RESOLVE_IPADDR=${1}
 while [ 1 ]
 do
         echo "############ START CONNECT #################"
-        hakoniwa_proxy ./proxy_param.json ${RESOLVE_IPADDR}  50051
+        hakoniwa_proxy ./proxy_param-mac.json ${RESOLVE_IPADDR}  50051
         echo "############ SERVER DOWN #################"
         sleep 1
 done
